c71f54f6a55dda8b1cd60e66388be451fcc9aaf1
jaycrypto <jaycrypto@gmail.com>
2016-01-13 18:01:20 +0000

    all done

---
+++ README.md
@@ -0,0 +1,11 @@
+# File Metadata Microservice
+
+Here are the specific user stories you should implement for this Basejump:
+
+- User Story: I can submit a FormData object that includes a file upload.
+
+- User Story: When I submit something, I will receive the file size in bytes within the JSON response
+
+Hint: You may want to use this package: https://www.npmjs.com/package/multer
+
+Try: https://file-metadata-jaycrypto.c9users.io
---
+++ server.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var express = require("express");
+var multer = require("multer");
+
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+
+var upload = multer({ 
+  storage: storage,
+  limits: { fileSize: 3000000 }
+});
+
+var app = express();
+
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + '/public/index.html');
+});
+
+app.post('/upload', upload.single('img'), function (req, res) {
+  // console.log(req.body) // form fields
+  // console.log(req.file) // form files
+  res.json(req.file);
+  
+});
+
+var port = process.env.PORT || 8080; 
+app.listen(port, function() {
+  console.log("Server running at https://file-metadata-jaycrypto.c9users.io");
+});
